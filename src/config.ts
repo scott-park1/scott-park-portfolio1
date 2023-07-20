550bb854e1862df2ab752ba3eb89845ee568a409
scott.park1 <kcp951130@gmail.com>
2023-07-20 23:09:48 +1200

    Add header to scott and site description

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Scott Park Portfolio'
-export const SITE_DESCRIPTION = 'Portfolio of Scott Park.'
+export const SITE_DESCRIPTION =
+  'Portfolio of Scott Park that includes description, projects and blogs, cv, and links to GitHub and LinkedIn.'
